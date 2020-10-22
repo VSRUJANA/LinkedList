f47556b103182277642f94a917fc1d503febc1d1
VSRUJANA <srujana.valavala@gmail.com>
2020-10-22 23:31:50 +0530

    [srujana] Add. Welcome message added

--- DataStructure_LinkedList/Program.cs
+++ DataStructure_LinkedList/Program.cs
@@ -6,7 +6,7 @@ namespace DataStructure_LinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Linked List Program");
         }
     }
 }
